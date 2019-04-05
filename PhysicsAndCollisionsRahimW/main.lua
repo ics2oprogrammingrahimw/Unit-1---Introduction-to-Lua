3d7f7b14be791b0cb5599b18d298e9f4c3cc3345
Rahim Williams <47784173+s437374@users.noreply.github.com>
2019-04-05 08:55:00 -0400

    added

--- PhysicsAndCollisionsRahimW/main.lua
+++ PhysicsAndCollisionsRahimW/main.lua
@@ -19,6 +19,7 @@ ground.width = display.contentWidth
 physics.addBody(ground, "static", {friction=0.5, bounce=0.3})
 
 local beam = display.newImage("Images/beam.png")
+beam:scale(1,4)
 
 beam.x = display.contentCenterX/5
 beam.y = display.contentCenterY*1.65
@@ -37,30 +38,60 @@ local bkg = display.newImage("Images/bkg.png", 0, 0)
 bkg.x = display.contentCenterX
 bkg.y = display.contentCenterY
 
-bkg.x = display.contentWidth
-bkg.y = display.contentHeight
+bkg.width = display.contentWidth/2
+bkg.height = display.contentHeight/2
 
 bkg:toBack()
 
+bkg:scale(3,3)
 local function firstball()
 	
 	local ball1 = display.newImage("Images/super_ball.png", 0, 0)
 
-	physics.addBody(ball1, {density=1.0, friction=0.5, bounce=0.3, radius=25})
+	physics.addBody(ball1, {density=1.0, friction=0.7, bounce=0.9, radius=25})
 end
 
 local function secondBall()
 
-	local ball2 = display.newimage("Images/super_ball.png", 0, 0)
+	local ball2 = display.newImage("Images/super_ball.png", 1,0)
+
+	
+	ball2:scale(2, 2)
 
 	physics.addBody(ball2, {density=1.0, friction=0.5, bounce=0.3, radius=12.5})
-
-	ball2:scale(0.5, 0.5)
 end
 
-timer.performWithDelay( 0, firstball)
 
-timer.performWithDelay( 500, secondball)
+
+local function thirdball()
+
+	local Ball3 = display.newImage("Images/super_ball.png", 1, 0)
+
+	Ball3:scale(2.5,2.5)
+
+	physics.addBody(Ball3, {density=1.5, friction=0.7, bounce=0.6, radius=80})
+end
+
+
+local function FourthBall()
+
+	local Ball4 = display.newImage("Images/super_ball.png", 1, 0)
+
+	Ball4:scale(2.2, 2.2)
+
+	physics.addBody(Ball4, {density=1.0, friction=0.2, bounce=0.6})
+end
+
+
+
+timer.performWithDelay( 0, thirdball)
+
+timer.performWithDelay( 1000, firstball)
+
+timer.performWithDelay( 500, secondBall)
+
+timer.performWithDelay( 700, FourthBall)
+
 
 
 
