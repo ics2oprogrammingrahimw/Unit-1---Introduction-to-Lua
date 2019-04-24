3eafae42fd484170246eee87e875f09928e85203
Rahim Williams <47784173+s437374@users.noreply.github.com>
2019-04-24 08:31:28 -0400

    fixes

--- AreaRectangleCircleRahimW/main.lua
+++ AreaRectangleCircleRahimW/main.lua
@@ -1,7 +1,8 @@
 -- Title: AreaOfRectangleAndCircle
 -- Name: Your Name
 -- Course: ICS2O/3C
--- This program....displays a colored rectangle with a border of a different color. It also calculates the rectangles area  and displays it on the screen. Then i added the code to draw a circle of a different color. i used the radius to calculate the area and displayed this on the iPad
+-- This program....displays a colored rectangle with a border of a different color. It also calculates the rectangles area  and displays it on the screen.
+-- Then i added the code to draw a circle of a different color. i used the radius to calculate the area and displayed this on the iPad
 local areaText
 local textsize = 50
 local myRectangle
@@ -10,10 +11,14 @@ local heightOfRectangle = 201
 local AreaOfRectangle
 
 --set backround color of the screen.Remember that colors are between 0 and 1.
-display.setDefault("background", 62/255, 110/255, 207/255)
+display.setDefault("background", 78/255, 110/255, 100/255)
+
+--removing the status bar
+display.setStatusBar(display.HiddenStatusbar)
+
 
 --draw the rectangle that is half the width and height of the screen size
-myRectangle = display.newRect(350, 300, widthOfRectangle, heightOfRectangle)
+myRectangle = display.newRect(0, 0, widthOfRectangle, heightOfRectangle)
 
 -- anchor the rectangle in the top left corner on the screen and set its (x,y) position
 myRectangle.anchorX = 0
@@ -25,7 +30,7 @@ myRectangle.y = 20
 myRectangle.strokeWidth = 20
 
 -- set the color of the rectangle
-myRectangle:setFillColor(0.7, 0.1, 0.3)
+myRectangle:setFillColor(0.9, 0.1, 0.3)
 
 --set the color of the border 
 myRectangle:setStrokeColor(0, 1, 0)
@@ -35,8 +40,9 @@ AreaOfRectangle = widthOfRectangle * heightOfRectangle
 
 -- write the area on the screen. take into consideration the size of the font when positioning it on the screen
 areaText = display.newText("The area of this rectangle with a width of \n" .. 
+
 	widthOfRectangle .. " and a height of " .. heightOfRectangle .. " is " .. 
-	AreaOfRectangle .. " pixels²." 0, 0, (Arial, textSize)
+	AreaOfRectangle .. " pixels².", 0, 0, Arial, textSize)
 
 --anchor the text and set its (x,y) position
 areaText.anchorX = 0
